eff3fa2abbde699843215a6e185747efe9e8047d
Aaron Hill <aa1ronham@gmail.com>
2019-09-28 15:57:17 -0400

    Add fd sending code

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,28 @@
 use std::os::unix::net::UnixDatagram;
-use std::os::unix::io::RawFd;
+use std::os::unix::io::{RawFd, AsRawFd};
 use std::mem;
 
-// Based on https://stackoverflow.com/a/2358843/1290530
-/*fn send_fd(fd: RawFd, sock: &UnixDatagram) {
-    let dummy: libc::c_char = '$';
-    let iov = libc::iovec {
-        iov_base: &dummy,
-        iov_len: std::mem::size_of_val(&dummy);
-    };
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
-    let msg = libc::msghdr {
-        msg_name: 0,
-        msg_namelen: 0,
-        msg_iov: &iov,
-        msg_iovlen: 1,
-        msg_control: 
-    };
-}*/
+// Based on https://stackoverflow.com/a/2358843/1290530
+fn send_fd(fd: RawFd, sock: &UnixDatagram) {
+    let mut msg: libc::msghdr = unsafe { mem::zeroed() };
+    let fds: [libc::c_int; 1] = [fd];
+    let mut buf: [libc::c_char; ONE_FD_BUF_SIZE] = unsafe { mem::zeroed() };
+    let fd_ptr: *mut libc::c_int;
+
+    msg.msg_control = buf.as_mut_ptr() as *mut libc::c_void;
+    msg.msg_controllen = mem::size_of_val(&buf);
+    let cmsg: *mut libc::cmsghdr; 
+    unsafe {
+        cmsg = libc::CMSG_FIRSTHDR(&msg);
+        (*cmsg).cmsg_level = libc::SOL_SOCKET;
+        (*cmsg).cmsg_type = libc::SCM_RIGHTS;
+        (*cmsg).cmsg_len = libc::CMSG_LEN((mem::size_of::<libc::c_int>() * 1) as libc::c_uint) as libc::size_t;
+        fd_ptr = libc::CMSG_DATA(cmsg) as *mut libc::c_int;
+        *fd_ptr = fds[0];
+        msg.msg_controllen = (*cmsg).cmsg_len;
+
+        libc::sendmsg(sock.as_raw_fd(), &msg, 0);
+    }
+}
--- src/wrapper.h
+++ src/wrapper.h
@@ -1,4 +1,4 @@
 #include <sys/socket.h>
 
 static int fd_buf[1];
-const unsigned long int ONE_FD_BUF_SIZE = CMSG_SPACE(sizeof fd_buf);
+const size_t ONE_FD_BUF_SIZE = CMSG_SPACE(sizeof fd_buf);
